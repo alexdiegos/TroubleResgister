688d639105de7f449a3d68d5800911a44664d42c
Alexandre Silva <alexandre.silva@digitallworkers.com>
2022-07-23 19:08:41 -0300

    Initial Commit

---
+++ TroubleRegister/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TroubleRegister
+//
+//  Created by Alexandre Silva on 23/07/22.
+//  Copyright © 2022 AlexDiegoS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
